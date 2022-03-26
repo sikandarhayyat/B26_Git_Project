1123609957985b20111cbc977952d73af1135637
sikandarhayyat <sikandrahayyat@gmail.com>
2022-03-26 15:57:52 -0400

    Hello World

--- src/day0/HelloWorld.java
+++ src/day0/HelloWorld.java
@@ -2,6 +2,6 @@ package day0;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hellow World");
+        System.out.println("Hello World");
     }
 }
